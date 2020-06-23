db51e6c218309f72a75d44842b4c0a07e80ed005
Daniel Garnier-Moiroux <git@garnier.wf>
2020-06-23 14:29:48 +0200

    Setup express to use passport

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 const passport = require('./setup-passport');
 const app = require('express')();
 
+// Setup passport dependencies + middleware
+app.use(require('body-parser').urlencoded({ extended: true }));
+app.use(require('express-session')({ secret: 'zuper-zecret-key', resave: false, saveUninitialized: false }));
+app.use(passport.initialize());
+app.use(passport.session());
+
 app.get('/', (req, res, next) => {
   res.send('hello world');
 });
